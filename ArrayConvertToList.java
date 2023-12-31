9e667dceb1b819051bbc52e216b82e9903b2a946
Adapanavya <151008056+Adapanavya@users.noreply.github.com>
2023-12-31 14:49:03 +0530

    Add files via upload

---
+++ ArrayConvertToList.java
@@ -0,0 +1,16 @@
+package com.java.practice.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayConvertToList {
+    public static void main(String[] args){
+        List<String> arrayToList=new ArrayList<>();
+        arrayToList.add("navya");
+        arrayToList.add("adapa");
+        arrayToList.add("xyz");
+        //for (int i=0; i>= arrayToList.size()-1; i--){
+            System.out.println(arrayToList);
+
+        }
+    }
